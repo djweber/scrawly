a02612ffb8666e0b74e9a3600a1d7380bd4b624c
David Weber <djw612@gmail.com>
2014-09-08 01:47:04 -0400

    Color well generation

--- src/modules/tray.js
+++ src/modules/tray.js
@@ -1,27 +1,35 @@
+function setColors(colors) {
+    /* Populate tray with colors (switch to color picker later) */
+    console.log(colors);
+    var width = parseInt(window.getComputedStyle(this.container, null).getPropertyValue('width'));
+    var padding = parseInt(window.getComputedStyle(this.container, null).getPropertyValue('padding'));
+    var margin = ((width - padding) / colors.length) / 4;
+    console.log(margin);
+    colors.forEach(function(v,i,a) {
+        var well = document.createElement('div');
+        well.className = 'well';
+        well.style.backgroundColor = v;
+        well.style.marginLeft = margin + 'px';
+        well.style.marginRight = margin +'px';
+        this.container.appendChild(well);
+    }.bind(this));
+
+    /* Bind single event listener to canvas (event delegation) */
+    document.addEventListener('click', function(e) {
+        if(e.target.className == 'well') {
+            console.log(e.target.style.backgroundColor);
+            this.selColor = e.target.style.backgroundColor;
+        }
+    }.bind(this));
+}
+
 module.exports = function(el) {
     var element = document.getElementById(el);
     return {
         container: element,
         wells: [],
         selColor: '#ac4142',
-        setColors: function(colors) {
-            /* Populate tray with colors (switch to color picker later) */
-            console.log(colors); 
-            colors.forEach(function(v,i,a) {
-                var well = document.createElement('div');
-                well.className = 'well';
-                well.style.backgroundColor = v;
-                this.container.appendChild(well);
-            }.bind(this));
-
-            /* Bind single event listener to canvas (event delegation) */
-            document.addEventListener('click', function(e) {
-                if(e.target.className == 'well') {
-                    console.log(e.target.style.backgroundColor);
-                    this.selColor = e.target.style.backgroundColor;
-                }
-            }.bind(this));
-        }, 
+        setColors: setColors, 
         clear: function() {
         
         }
